7fbb763691f8c5ce8653195a599c96c3afb014a8
ivyzbot <ivyxiao83@gmail.com>
2024-02-04 19:30:46 +0800

    add product list in frontedn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css'
+import './App.css';
+import { sampleProducts } from './assets/data';
 
 function App() {
   return (
     <div>
       <header>TS Amazon</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                className="product-image"
+                src={product.image}
+                alt={product.name}
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
